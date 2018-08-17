dc9a518dcbd0d3e5ec630e3d0505b0fa5a799683
Arshpreet Kaur <arshpreetkaur989@gmail.com>
2018-08-17 11:18:33 -0500

    Initial commit

---
+++ exercises/Controllers/ExercisesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace exercises.Controllers
+{
+    public class ExercisesController : Controller
+    {
+        // GET: Exercise
+        public ActionResult JSExercises()
+        {
+            return View();
+        }
+    }
+}
---
+++ exercises/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="exercises.MvcApplication" Language="C#" %>
